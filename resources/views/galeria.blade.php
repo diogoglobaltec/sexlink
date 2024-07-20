a7a04e56a753c1d53af341881cf0b54362978c0e
z3r0 <diogoglobaltec@gmail.com>
2024-07-20 09:17:37 -0300

    design galeria

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -5,16 +5,13 @@
 @section('NomeTelefone')
 
 
-
-
-
 @foreach ($saida2 as $user)
 
 
 
-
-
-
+<div class="container">
+  <div class="row">
+    <div class="col">
 
 <?php
 //Limpa caracteres do telefone.
@@ -24,18 +21,48 @@ $phone = preg_replace('/\D+/', '', $tel);
 
 <a href= "{{ url('perfil/'.$user->id_acompanhante.'') }}">
 <img src="<?php echo asset("/images/$user->foto")?>" id="imgaleria" width="180" onmouseover="demoVisibility()"><br>
- {{ $user->username  }} <br></a>
+<br></a>
 <br>
-  <a href="{{ url( 'https://wa.me/55'.$phone) }}">
+ <p>
+
+
+
+
+
+    </div>
+    <div class="col">
+Nome:{{$user->username}} <br>
+Cidade: {{$user->cidade}} <br>
+Região:{{$user->regiao }} <br>
+Telefone:{{$phone }}
+ <a href="{{ url( 'https://wa.me/55'.$phone) }}"><br>
 <button>
 whatsapp
 </button>
-</a><p>
+</a>
+
+    </div>
+  </div>
+</div>
+
+
+
+<div class="row">
+<div class="col">
+
+
+<!-- Div de fechamento da galeria -->
+</div>
+</div>
+<!-- Div de fechamento da galeria -->
+
 <hr>
 
 
 @endforeach
 @stop
+
+
 
 
 @section('galeriaOuro')
@@ -44,8 +71,8 @@ whatsapp
 
 
 <a href= "{{ url('perfil/'.$testar->id_acompanhante.'') }}">
+<img src="<?php echo asset("/images/$testar->foto")?>" id="imgaleria" width="180" onmouseover="demoVisibility()"></a><br>
 
-<img src="<?php echo asset("/images/$testar->foto")?>" id="imgaleria" width="280" onmouseover="demoVisibility()"><br>
 
 <?php
 //Limpa caracteres do telefone.
@@ -53,15 +80,17 @@ $tel = $testar->telefone;
 $phone = preg_replace('/\D+/', '', $tel);
 ?>
 
-
-        {{ $testar->username  }} <br></a>
-         <a href="{{ url( 'https://wa.me/55'.$phone) }}">
-
-
+Cidade:{{ $testar->cidade }} - Região:{{ $testar->regiao }} <br>
+<a href="{{ url( 'https://wa.me/55'.$phone) }}">
 <button>
 whatsapp
 </button>
+</a>
 
+
+
+<div>
+</div>
 
 
         @endforeach
