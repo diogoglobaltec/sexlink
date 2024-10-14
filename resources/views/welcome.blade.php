02204906de3ea1e8f18e9e09b79fe9503b102f0b
z3r0 <diogoglobaltec@gmail.com>
2024-10-14 19:20:04 -0300

    modificação welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,63 +53,111 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-
                 </div>
+
+
+
+
+
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
-<div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('galeria') }}" class="underline text-gray-900 dark:text-white">Galeria Acompanhantes</a></div>
-
+<div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('galeria') }}" class="underline text-gray-900 dark:text-white">Galeria Garotas</a></div>
                             </div>
 
                             <div class="ml-12">
-
                             </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                            <div class="flex items-center">
-
                             </div>
+
+<div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+<div class="flex items-center">
+<div class="ml-4 text-lg leading-7 font-semibold"><a href="" class="underline text-gray-900 dark:text-white">Grupo Telegram<br> Garotas.</a>
+
+</div>
+</div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 
+
+
                                 </div>
                             </div>
                         </div>
+
+
+
+
+
+
+
+
+
+
+
+              <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                   <div class="flex items-center">
+                      <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('MulherTrans') }}" class="underline text-gray-900 dark:text-white">.......teste</a></div>
+                            </div>
+
+
+
+
+                            <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+
+                            </div>
+                            </div>
+                            </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                           <div class="flex items-center">
+                              <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+
+
+                            teste ...........
+
+                            </div>
+                            </div>
+
+
+
+                                <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+
+                                </div>
+                                </div>
+                                </div>
+
+
+
+
+
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="register" class="underline text-gray-900 dark:text-white">Anuncie conosco </a></div>
-                            </div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ route('MulherTrans') }}" class="underline text-gray-900 dark:text-white">..............</a>
+
+                                                </div>
+                                                </div>
+                                                </div>
 
 
 
-
-
-
-                            <div class="ml-12">
+  <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 
                                 </div>
                             </div>
                         </div>
 
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                            <div class="flex items-center">
 
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">...</div>
-                            </div>
 
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 
-                                </div>
-                            </div>
-                        </div>
+
+
                     </div>
                 </div>
 
@@ -122,7 +170,6 @@
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
 
-                                            site by: z3r0
 
                 </div>
                 </div>
